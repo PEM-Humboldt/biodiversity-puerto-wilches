817464a62256f106b400eb5ba8539c4e0e4ed285
marbotte <bottinmarius@gmail.com>
2023-04-20 23:05:57 -0500

    Suppressed R files in the analyses folder

--- Analyses/redo_bioEnv_hellinger.R
+++
@@ -1,8 +0,0 @@
-require(rmarkdown)
-require(foreach)
-require(doParallel)
-(FilesRMD <- dir(pattern=".*bioEnv.*.Rmd",recursive=T))
-for(i in paste(getwd(),c("Hidrobiologicos/macroinvertebrados_bioEnv_RDA.Rmd","Hidrobiologicos/zooplancton_bioEnv_RDA_hellinger.Rmd"),sep="/")){
-  setwd(dirname(i))
-  render(i)
-}
--- Analyses/redo_bioenv.R
+++
@@ -1,8 +0,0 @@
-require(rmarkdown)
-require(foreach)
-require(doParallel)
-(FilesRMD <- dir(pattern=".*bioEnv.*.Rmd",recursive=T))
-for(i in paste(getwd(),c("Hidrobiologicos/macrofitas_bioEnv_RDA.Rmd","Hidrobiologicos/zooplancton_bioEnv_RDA.Rmd","Peces/peces_bioEnv_RDA.Rmd"),sep="/")){
-  setwd(dirname(i))
-  render(i)
-}
